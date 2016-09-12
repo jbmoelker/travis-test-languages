218abe994d27ae812912435401c0b4a78c32cd23
Jasper Moelker <j.b.moelker@activeids.nl>
2016-09-12 16:59:08 +0200

    use a composer package in php test

--- test.php
+++ test.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "hello from PHP script!";
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Array(array(
+    'index' => 'Hello from a {{ lang }} script!',
+));
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index', array('lang' => 'PHP'));
